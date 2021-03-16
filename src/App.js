56f0a15fc5db1c234c9bc11bc865391b5abca8c1
Sabbir Ahmmed <sabbir.cse@yahoo.com>
2021-03-16 11:30:57 +0600

    sign out & bootstrap add

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import firebase from "firebase/app";
 import "firebase/auth";
-import firebaseConfig from './Firebase.config';
+import firebaseConfig from './firebase.config';
 import { useState } from 'react';
 
 firebase.initializeApp(firebaseConfig);
@@ -9,6 +9,7 @@ firebase.initializeApp(firebaseConfig);
 
 function App() {
   const provider = new firebase.auth.GoogleAuthProvider();
+
   const [user, setUser] = useState({
     isUserSignIn: false,
     name:'',
@@ -29,11 +30,36 @@ function App() {
       }
       setUser(signedInUser);
     })
+    .catch(err =>{
+      console.log(err);
+      console.log(err.message);
+    })
+  }
+
+  const handleSignOut = () =>{
+    firebase.auth().signOut()
+    .then(res => {
+      const UserSignOut = {
+        isUserSignIn: false,
+        name:'',
+        email:'',
+        photo:''
+      }
+      setUser(UserSignOut);
+    })
+    .catch( err => {
+      console.log(err);
+    })
+
+    alert('User sign out successful !');
   }
 
   return (
-    <div>
-      <button onClick={handleSignIn}>Sign in</button>
+    <div className='App'>
+      {
+        user.isUserSignIn ? <button className='btn btn-warning  mb-5' onClick={handleSignOut}>Sign Out</button> :
+        <button className='btn btn-primary' onClick={handleSignIn}>Sign in</button>
+      }
       {
         user.isUserSignIn && <div>
           <p>Welcome, {user.name}</p>
